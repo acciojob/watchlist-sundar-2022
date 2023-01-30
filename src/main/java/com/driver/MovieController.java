9cc2ecf70cd33d5815e93f5f5795e6054ed7ce6e
Mr SUNDAR <m.n.sundar.2022@gmail.com>
2023-01-30 11:27:45 +0530

    solution 5

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.util.List;
 @RestController
+@RequestMapping("movies")
 public class MovieController {
 
     @Autowired
